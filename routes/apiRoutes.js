761d88f0ed2cd1425a06228cb5e12c2d8bc21c82
Ari Horowitz <ariehh@comcast.net>
2020-02-15 12:16:00 -0800

    app.delete function in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,29 +3,45 @@
 const path = require("path");
 const fs = require("fs");
 
-
 module.exports = app => {
-app.get("/api/notes", function (req, res) {
+  app.get("/api/notes", function(req, res) {
     fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
-        if (err) throw err;
-        console.log(JSON.parse(data));
-        res.json(JSON.parse(data));
-      });
+      if (err) throw err;
+      console.log(JSON.parse(data));
+      res.json(JSON.parse(data));
     });
+  });
 
-app.post("/api/notes", function (req, res) {
+  app.post("/api/notes", function(req, res) {
     fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
-        if (err) throw err;
-        let newNote = req.body;
-        let notesArr = (JSON.parse(data));
-        let id = notesArr[notesArr.length - 1].id + 1;
-        newNote.id = id;
-        notesArr.push(newNote);
-        let notesString = JSON.stringify(notesArr);
-        console.log(typeof notesString);
-        fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString)
-      });
+      if (err) throw err;
+      let newNote = req.body;
+      let notesArr = JSON.parse(data);
+      let id = notesArr[notesArr.length - 1].id + 1;
+      newNote.id = id;
+      notesArr.push(newNote);
+      let notesString = JSON.stringify(notesArr);
+      console.log(typeof notesString);
+      fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString);
     });
+  });
 
-
-app.delete()
+  app.delete("/api/notes/:id", function(req, res) {
+    fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+      if (err) throw err;
+      console.log(req.params.id);
+      //console.log(JSON.parse(data));
+      let notesArr = JSON.parse(data);
+      let newNotesArr = [];
+      for (i = 0; i < notesArr.length; i++) {
+        if (notesArr[i].id != req.params.id) {
+          newNotesArr.push(notesArr[i]);
+        }
+      }
+      console.log(newNotesArr);
+      let notesString = JSON.stringify(newNotesArr);
+      console.log(notesString);
+      fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString);
+    });
+  });
+};

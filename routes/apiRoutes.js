0624c2cb375484b71f04482f4d5b08b834be5a2d
Ari Horowitz <ariehh@comcast.net>
2020-02-15 12:11:51 -0800

    app.post function in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,6 +17,15 @@ app.post("/api/notes", function (req, res) {
     fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
         if (err) throw err;
         let newNote = req.body;
+        let notesArr = (JSON.parse(data));
+        let id = notesArr[notesArr.length - 1].id + 1;
+        newNote.id = id;
+        notesArr.push(newNote);
+        let notesString = JSON.stringify(notesArr);
+        console.log(typeof notesString);
+        fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString)
+      });
+    });
 
 
 app.delete()

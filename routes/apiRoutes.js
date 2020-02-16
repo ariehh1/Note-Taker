7ea8a13ee2f65344932c970493c2e69dc1b4deb6
Ari Horowitz <ariehh@comcast.net>
2020-02-16 12:07:42 -0800

    db.json file rewrites after new note is saved

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,4 +21,27 @@ module.exports = function(app) {
     notesData = getNotes();
     res.json(notesData);
   });
+
+  app.post("/api/notes", function(req, res) {
+    notesData.push(req.body);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesData), "utf8");
+    res.json(true);
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    const requestID = req.params.id;
+    console.log(requestID);
+
+    let note = notesData.filter(note => {
+      return note.id === requestID;
+    })[0];
+
+    console.log(note);
+    const index = notesData.indexOf(note);
+
+    notesData.splice(index, 1);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesData), "utf8");
+    res.json("Note deleted");
+  });
 };
